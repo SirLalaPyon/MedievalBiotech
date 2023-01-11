16dc64dd22c1cb4ac569415359a7e5331be44198
SirLalaPyon <76900263+SirLalaPyon@users.noreply.github.com>
2023-01-12 02:38:29 +0800

    initial commit

---
+++ Source/ConsumableGenepack.cs
@@ -0,0 +1,36 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using Verse;
+
+namespace ConsumableGenepack {
+    public class ConsumableGenepack : ThingComp {
+        public CompProperties_ConsumableGenepack Props => (CompProperties_ConsumableGenepack)props;
+        public override void PostIngested(Pawn ingester) {
+            if (ingester.RaceProps.IsFlesh) {
+                if (ModsConfig.BiotechActive && ingester.genes != null) {
+                    Genepack pack = parent as Genepack;
+                    if (pack != null) {
+                        foreach (GeneDef gene in pack.GeneSet.GenesListForReading) {
+                            if (!ingester.genes.HasGene(gene)) {
+                                ingester.genes.AddGene(gene, true);
+                            }
+                        }
+                    }
+                    if (Props.genes.Any()) {
+                        foreach (GeneDef gene in Props.genes) {
+                            if (!ingester.genes.HasGene(gene)) {
+                                ingester.genes.AddGene(gene, true);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    public class CompProperties_ConsumableGenepack : CompProperties {
+        public List<GeneDef> genes = new List<GeneDef>();
+        public CompProperties_ConsumableGenepack() {
+            compClass = typeof(ConsumableGenepack);
+        }
+    }
+}
